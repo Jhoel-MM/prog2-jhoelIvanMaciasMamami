cfe3532e971638bdf1e855bd0c89ba1fa002a0a2
Jhoel Ivan Macias Mamani <43515294-jhoelmacias30@users.noreply.replit.com>
2025-06-09 19:45:22 +0000

    Ejercicio condicionales: verificador de edad para peliculas

--- clase02_mi_codigo.py
+++ clase02_mi_codigo.py
@@ -1,3 +1,11 @@
-print(¡Estoy aprendiendo Git y Github en prog II)
-nombre = jhoel Ivan Macias Mamani
-print(feste commit lo esta haciendo: {nombre})
+edad_str = input("Bienvenido al cine, ¿cuál es tu edad?: ")
+edad = int(edad_str)
+
+if edad < 0:
+    print("Edad no válida. Por favor, ingresa un número positivo.")
+elif edad >= 18:
+    print("¡Puedes ver películas clasificadas R!")
+elif edad >= 13:
+    print("Puedes ver películas clasificadas PG-13.")
+else:
+    print("Te recomendamos películas clasificadas G o PG.")
